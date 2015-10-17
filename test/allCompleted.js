0a515a723076f4cbdad9832414bc59d4f063ab1e
Jacob Blakely <jacob@codekirei.com>
2015-10-16 17:33:47 -0700

    write tests for allCompleted method

--- test/allCompleted.js
+++ test/allCompleted.js
@@ -1,4 +1,33 @@
+'use strict'
+
+//----------------------------------------------------------
+// Modules
+//----------------------------------------------------------
+// NPM
+const assert = require('chai').assert
+
+// Local
+const Multispinner = require('../')
+const genSpinners = require('./utils/genSpinners')
+
+//----------------------------------------------------------
+// Tests
+//----------------------------------------------------------
 describe('allCompleted method', () => {
-  it('Return false if not all spinners are complete')
-  it('Return true if all spinners are complete')
+  // fixtures
+  let spinners = genSpinners.arr(3)
+
+  it('Return false if not all spinners are complete', () => {
+    let multispinner = new Multispinner(spinners)
+    multispinner.success(spinners[0])
+    assert.isFalse(multispinner.allCompleted())
+  })
+
+  it('Return true if all spinners are complete', () => {
+    let multispinner = new Multispinner(spinners)
+    spinners.map(spinner => {
+      multispinner.error(spinner)
+    })
+    assert.isTrue(multispinner.allCompleted())
+  })
 })
---
+++ test/utils/genSpinners.js
@@ -0,0 +1,48 @@
+'use strict'
+
+//----------------------------------------------------------
+// Modules
+//----------------------------------------------------------
+// NPM
+const faker = require('faker')
+
+// Local
+
+//----------------------------------------------------------
+// Logic
+//----------------------------------------------------------
+/**
+ * 
+ * @param {} 
+ */
+function arrayOfSpinners(count) {
+  let arr = []
+  let i = 0
+  while (i < count) {
+    arr.push(faker.fake('{{name.firstName}}'))
+    i++
+  }
+  return arr
+}
+
+/**
+ * 
+ * @param {} 
+ */
+function objectOfSpinners(count) {
+  let obj = {}
+  let i = 0
+  while (i < count) {
+    object[faker.fake('{{name.firstName}}')] = faker.fake('{{lorem.sentence}}')
+    i++
+  }
+  return obj
+}
+
+//----------------------------------------------------------
+// Exports
+//----------------------------------------------------------
+module.exports = {
+  arr: arrayOfSpinners,
+  obj: objectOfSpinners
+}
