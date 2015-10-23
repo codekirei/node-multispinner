c5bc76d2dde732f58fb710e03f5944a75e436b90
Jacob Blakely <jacob@codekirei.com>
2015-10-22 21:09:49 -0700

    fix and modularize tests for stop method

--- test/methods.js
+++ test/methods.js
@@ -4,23 +4,7 @@
 // Modules
 //----------------------------------------------------------
 // NPM
-const assert = require('chai').assert
-const sinon = require('sinon')
-const logUpdate = require('log-update')
-const faker = require('faker')
-const clone = require('lodash.clonedeep')
 const path = require('path')
-
-// Local
-const Multispinner = require('../')
-const Spinners = require('lib/spinners')
-const defaultProps = require('lib/constants').defaultProps
-const states = require('lib/constants').states
-const validOpts = require('lib/validOpts')
-
-// Test Utils
-const types = require('./utils/types')
-const genSpinners = require('./utils/genSpinners')
 
 //----------------------------------------------------------
 // Tests
@@ -33,32 +17,10 @@ describe('Methods', () => {
     'success',
     'error',
     'allCompleted',
-    'start'
+    'start',
+    'stop'
   ]
   methods.map(method => {
     require(`./${path.join('methods', method)}`)
-  })
-  //----------------------------------------------------------
-  // stop
-  //----------------------------------------------------------
-  describe('stop', () => {
-    // setup
-    const spinners = genSpinners.arr(3)
-    let m
-    beforeEach(() => {
-      m = new Multispinner(spinners, {debug: true})
-      m.start()
-    })
-    afterEach(() => {
-      m.cleanUp()
-    })
-
-    it('Clear interval bound to this.state', () => {
-      // setup
-      m.stop()
-
-      // assertions
-      assert.equal(-1, m.state._idleTimeout)
-    })
   })
 })
---
+++ test/methods/stop.js
@@ -0,0 +1,38 @@
+'use strict'
+
+//----------------------------------------------------------
+// Modules
+//----------------------------------------------------------
+// NPM
+const assert = require('chai').assert
+const sinon = require('sinon')
+
+// Local
+const Multispinner = require('../../')
+
+// Test Utils
+const genSpinners = require('../utils/genSpinners')
+
+//----------------------------------------------------------
+// Tests
+//----------------------------------------------------------
+module.exports = describe('stop', () => {
+  it('Clear interval bound to this.state', () => {
+    const spinners = genSpinners.arr(3)
+    const m = new Multispinner(spinners, {testing: true})
+    const props = [
+      '_onTimeout',
+      '_idlePrev',
+      '_idleNext',
+      '_repeat'
+    ]
+    m.start()
+    m.stop()
+
+    // assertions
+    assert.equal(-1, m.state._idleTimeout)
+    props.map(prop => {
+      assert.isNull(m.state[prop])
+    })
+  })
+})
