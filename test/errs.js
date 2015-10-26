536ce31531b5663ba48f97631aa60dd6b9288a94
Jacob Blakely <jacob@codekirei.com>
2015-10-25 19:36:30 -0700

    write tests for validOpts errs

--- test/errs.js
+++ test/errs.js
@@ -7,10 +7,13 @@
 const assert = require('chai').assert
 const sinon = require('sinon')
 const faker = require('faker')
+const clone = require('lodash.clonedeep')
+const kindOf = require('kind-of')
 
 // Local
 const Multispinner = require('../')
 const Spinners = require('lib/spinners')
+const defaultProps = require('lib/constants').defaultProps
 const states = require('lib/constants').states
 const validOpts = require('lib/validOpts')
 
@@ -25,10 +28,28 @@ describe('errs', () => {
   const spinners = genSpinners.arr(3)
   const spinner = spinners[0]
   describe('validOpts', () => {
-    it('throw when opt is not a configurable property')
-    it('throw when opt is incorrect type')
+    it('throw when opt is not a configurable property', () => {
+      const goodOpts = clone(defaultProps)
+      assert.doesNotThrow(() => validOpts(goodOpts))
+      const badOpt = {a: 'a'}
+      assert.throws(() => validOpts(badOpt))
+    })
+    it('throw when opt is incorrect type', () => {
+      const opts = clone(defaultProps)
+      // test every opt
+      Object.keys(opts).map(opt => {
+        const validType = kindOf(opts[opt])
+        const invalidTypes = types.except(validType)
+        // for every invalid type
+        Object.keys(invalidTypes).map(type => {
+          const testOpt = {}
+          testOpt[opt] = invalidTypes[type]
+          assert.throws(() => validOpts(testOpt))
+        })
+      })
+    })
     it('throw when opts param is defined and not an object', () => {
-      const testTypes = types.except(['null', 'undefined', 'object',])
+      const testTypes = types.except(['null', 'undefined', 'object'])
       Object.keys(testTypes).map(type => {
         assert.throws(() => validOpts(testTypes[type]))
       })
