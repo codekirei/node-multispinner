d188817fa761b980ac3915dbc96a66a94252d344
Jacob Blakely <jacob@codekirei.com>
2015-10-25 20:40:31 -0700

    write tests for Spinners errs

--- test/errs.js
+++ test/errs.js
@@ -27,6 +27,9 @@ const types = require('./utils/types')
 describe('errs', () => {
   const spinners = genSpinners.arr(3)
   const spinner = spinners[0]
+  //----------------------------------------------------------
+  // validOpts errs
+  //----------------------------------------------------------
   describe('validOpts', () => {
     it('throw when opt is not a configurable property', () => {
       const goodOpts = clone(defaultProps)
@@ -34,20 +37,57 @@ describe('errs', () => {
       const badOpt = {a: 'a'}
       assert.throws(() => validOpts(badOpt))
     })
+
     it('throw when opt is incorrect type', () => {
-      const opts = clone(defaultProps)
-      // test every opt
-      Object.keys(opts).map(opt => {
+      /**
+       * @function invalidTypes
+       * @param {object} opts - default opts
+       * @param {string} opt - key of relevant opt
+       * @returns {object} types opt is not
+       */
+      function invalidTypes(opts, opt) {
         const validType = kindOf(opts[opt])
-        const invalidTypes = types.except(validType)
-        // for every invalid type
-        Object.keys(invalidTypes).map(type => {
-          const testOpt = {}
-          testOpt[opt] = invalidTypes[type]
-          assert.throws(() => validOpts(testOpt))
+        return types.except(validType)
+      }
+
+      /**
+       * @function testTypes
+       * @param {string} opt - key of relevant opt
+       * @param {object} typesToTest - types opt is not
+       * @returns {undefined}
+       */
+      function testTypes(opt, typesToTest) {
+        Object.keys(typesToTest).map(type => {
+          testType(opt, typesToTest[type])
         })
-      })
+      }
+
+      /**
+       * @function testType
+       * @param {string} opt - key of opt in question
+       * @param {*} type - typed entity to test
+       * @returns {undefined}
+       */
+      function testType(opt, type) {
+        const testObj = {}
+        testObj[opt] = type
+        assert.throws(() => validOpts(testObj))
+      }
+
+      /**
+       * @function testOpts
+       * @param {object} opts - opts to test 
+       * @returns {undefined}
+       */
+      function testOpts(opts) {
+        Object.keys(opts).map(opt => {
+          testTypes(opt, invalidTypes(opts, opt))
+        })
+      }
+
+      testOpts(clone(defaultProps))
     })
+
     it('throw when opts param is defined and not an object', () => {
       const testTypes = types.except(['null', 'undefined', 'object'])
       Object.keys(testTypes).map(type => {
@@ -55,16 +95,32 @@ describe('errs', () => {
       })
     })
   })
+  //----------------------------------------------------------
+  // spinners errs
+  //----------------------------------------------------------
   describe('spinners', () => {
-    it('throw when spinners param is not an object or array')
-    it('throw when spinners param is empty')
+    it('throw when spinners param is not an object or array', () => {
+      const invalidTypes = types.except(['object', 'array'])
+      Object.keys(invalidTypes).map(type => {
+        assert.throws(() => new Spinners(invalidTypes[type], '', '').spinners())
+      })
+    })
+    it('throw when spinners param is empty', () => {
+      const errs = [{}, []]
+      errs.map(err => {
+        assert.throws(() => new Spinners(err, '', '').spinners())
+      })
+    })
   })
+
+  //----------------------------------------------------------
+  // complete errs
+  //----------------------------------------------------------
   describe('complete', () => {
     it('throw when state is not a valid spinner state', () => {
       const m = new Multispinner(spinners, {testing: true})
       assert.throws(() => m.complete(spinner))
       assert.throws(() => m.complete(spinner, 'notValidState'))
-      // fixme -- assert doesn't throw if correct state
     })
   })
 })
