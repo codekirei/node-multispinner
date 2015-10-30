b6b7cdb165b91da4efde0a08943d9f29b51de680
Jacob Blakely <jacob@codekirei.com>
2015-10-30 15:05:10 -0700

    fix complete method tests

--- test/methods/complete.js
+++ test/methods/complete.js
@@ -5,7 +5,6 @@
 //----------------------------------------------------------
 // NPM
 const assert = require('chai').assert
-const sinon = require('sinon')
 
 // Local
 const Multispinner = require('../../')
@@ -24,14 +23,6 @@ module.exports = describe('complete', () => {
   let m
   beforeEach(() => {
     m = new Multispinner(spinners, {testing: true})
-    m.start()
-  })
-
-  it('Call stop method', () => {
-    let spy = sinon.spy(m, 'stop')
-    m.complete(spinner, states.success)
-    assert(spy.calledOnce, 'call stop method')
-    m.stop.restore()
   })
 
   it('Update spinner prop with state param', () => {
@@ -40,12 +31,5 @@ module.exports = describe('complete', () => {
     assert.equal(m.spinners[spinner].state, states.success)
     m.complete(spinner, states.error)
     assert.equal(m.spinners[spinner].state, states.error)
-  })
-
-  it('Call start method', () => {
-    let spy = sinon.spy(m, 'start')
-    m.complete(spinner, states.error)
-    assert(spy.calledOnce, 'call start method')
-    m.start.restore()
   })
 })
